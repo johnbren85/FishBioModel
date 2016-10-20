37c61b8808e93f5f569cc91c17b37c48fde11400
johnbren85 <johnsonbrent38@gmail.com>
2016-10-20 14:02:44 -0700

    Create test.py

---
+++ test.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python2.7
+
+
+import cgi, cgitb
+cgitb.enable() 
+
+print "Content-type:text/html\r\n\r\n"
+print '<html>'
+print '<head>'
+print '<title>This is where we get user input.</title>'
+print '</head>'
+print '<body>'
+print '<h2>Enter Values for Bioenergetics model</h2>'
+
+
+<form action="/nfs/stak/students/j/johnbren/testenv/Working.py" method="get">
+Total Daphnia: <input type="text" name="Total Daphnia">  <br />
+
+Daphnia Size: <input type="text" name="Daphnia Size" />  <br />
+
+Please Select Month: <select name="Month">
+<option value="March" selected>March</option>
+<option value="April">April</option>
+<option value="May">May</option>
+<option value="June">June</option>
+<option value="July">July</option>
+<option value="August">August</option>
+</select>
+<input type="submit" value="Submit"/>
+</form>
+ 
+print '</body>'
+print '</html>'
